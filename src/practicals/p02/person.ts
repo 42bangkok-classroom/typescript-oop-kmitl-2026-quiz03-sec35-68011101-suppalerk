4b8990992292a2e9d74621730ec6158f59fbf70e
68011101-suppalerk <68011101@kmitl.ac.th>
2026-02-16 09:49:31 +0700

    f1

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,25 @@
-export class Person {}
+export class Person {
+        firstname : string ;
+        lastname : string ;
+        private COUNTRY : string ;
+        age : number
+
+
+    constructor (firstname : string, lastname : string,age : number){
+        this.firstname = firstname
+        this.lastname = lastname
+        this.COUNTRY = 'THAILAND'
+        this.age = age
+
+    }
+        getFullName(): string {
+    return this.firstname +  this.lastname
+}
+
+getAge():number {
+    return this.age
+}
+setAge():number{
+return this.age
+}
+}
